977dab3a99428d004d57950322360c512af4fdce
William Bruntrager <w.bruntrager@gmail.com>
2019-03-11 08:48:41 +0100

    Updates

---
+++ precache-manifest.bd3a489278232d1c4dfc670133e1365d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "02ffdb182c4334c67dda",
+    "url": "/stock-returns/static/js/xlsx.02ffdb18.chunk.js"
+  },
+  {
+    "revision": "a86c411c4262a5d2ba3a",
+    "url": "/stock-returns/static/js/runtime~main.a86c411c.js"
+  },
+  {
+    "revision": "b91ccd1d4be34a7112ba",
+    "url": "/stock-returns/static/js/responsivedefaults.b91ccd1d.chunk.js"
+  },
+  {
+    "revision": "39b117d7931c6d0d557c",
+    "url": "/stock-returns/static/js/main.39b117d7.chunk.js"
+  },
+  {
+    "revision": "4145b9e6520ad32e9568",
+    "url": "/stock-returns/static/js/6.4145b9e6.chunk.js"
+  },
+  {
+    "revision": "afc4581036edf6c4e482",
+    "url": "/stock-returns/static/js/5.afc45810.chunk.js"
+  },
+  {
+    "revision": "6bc4521881e2ca1183db",
+    "url": "/stock-returns/static/js/4.6bc45218.chunk.js"
+  },
+  {
+    "revision": "f7332e079985cca557a4",
+    "url": "/stock-returns/static/js/3.f7332e07.chunk.js"
+  },
+  {
+    "revision": "39b117d7931c6d0d557c",
+    "url": "/stock-returns/static/css/main.53fc0841.chunk.css"
+  },
+  {
+    "revision": "798d8a4e885bf84fd9396f37f12c1298",
+    "url": "/stock-returns/index.html"
+  }
+];
---
+++ static/js/xlsx.02ffdb18.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{213:function(n,o){},222:function(n,o){},223:function(n,o){}}]);
+//# sourceMappingURL=xlsx.02ffdb18.chunk.js.map
